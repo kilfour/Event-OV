56a44d7f71267fef33d1cb5760d178d029d95303
kilfour <kilfour@gmail.com>
2024-11-28 20:01:10 +0100

    bestel nr in mollie transactie info

--- public/netlify/functions/create-checkout.js
+++ public/netlify/functions/create-checkout.js
@@ -13,7 +13,7 @@ exports.handler = async function (event, context) {
                 currency: 'EUR',
                 value: data.amount, // You must send the correct number of decimals, thus we enforce the use of strings
             },
-            description: 'EVENT OM 09/10/25',
+            description: 'BestelNr: ' + data.orderId,
             redirectUrl: 'https://pequivents.netlify.app/payment/' + data.orderId,
             // webhookUrl: 'https://webshop.example.org/payments/webhook/',
 
