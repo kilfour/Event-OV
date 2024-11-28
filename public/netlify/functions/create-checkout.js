3cbf26c0bd590dea32aee2c63b7511bc6a795924
kilfour <kilfour@gmail.com>
2024-11-28 17:20:37 +0100

    promises promises

--- public/netlify/functions/create-checkout.js
+++ public/netlify/functions/create-checkout.js
@@ -8,25 +8,17 @@ exports.handler = async function (event, context) {
         console.log(event.body);
 
         const mollieClient = createMollieClient({ apiKey: 'test_zPhwMGNRwTThJxUwGR9n4fJHtUVUaU' });
+        const payment = await mollieClient.payments.create({
+            amount: {
+                currency: 'EUR',
+                value: String(data.amount), // You must send the correct number of decimals, thus we enforce the use of strings
+            },
+            description: 'EVENT OM 09/10/25',
+            redirectUrl: 'https://pequivents.netlify.app/payment/' + data.orderId + '/',
+            // webhookUrl: 'https://webshop.example.org/payments/webhook/',
 
-        (async () => {
-
-            const payment = await mollieClient.payments.create({
-                amount: {
-                    currency: 'EUR',
-                    value: String(data.amount), // You must send the correct number of decimals, thus we enforce the use of strings
-                },
-                description: 'EVENT OM 09/10/25',
-                redirectUrl: 'https://pequivents.netlify.app/payment/' + data.orderId + '/',
-                // webhookUrl: 'https://webshop.example.org/payments/webhook/',
-                metadata: {
-                    order_id: data.orderId,
-                },
-            });
-
-            console.log(payment);
-
-        })();
+        });
+        console.log(payment);
         return {
             statusCode: 200,
             body: JSON.stringify(payment.getCheckoutUrl())
